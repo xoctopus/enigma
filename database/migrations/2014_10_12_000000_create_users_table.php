77ce9e0afdbb3454bf40d2f28ff31e9a5d671e1f
Leonardo Yanes <leo95batista@gmail.com>
2022-04-22 13:11:21 -0400

    Add 'active' column to 'User' table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,6 +27,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
