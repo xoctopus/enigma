d36d952e86c2a2021c15ab416ac630e1ff5874b6
Leonardo Yanes <leo95batista@gmail.com>
2022-04-22 13:29:53 -0400

    Create a mutator for the 'password' attribute on the 'user' model to ensure that the user's password is stored encrypted

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -42,4 +44,16 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'active' => 'boolean'
     ];
+
+    /**
+     * Interact with the user's password.
+     *
+     * @return Attribute
+     */
+    public function password(): Attribute
+    {
+        return Attribute::set(function ($password) {
+            return Hash::make($password);
+        });
+    }
 }
