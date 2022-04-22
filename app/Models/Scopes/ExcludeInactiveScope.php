58056d74db3969b99ab51e88b3fab95f21d4cf02
Leonardo Yanes <leo95batista@gmail.com>
2022-04-22 15:19:52 -0400

    Create a global query scope 'ExcludeInactiveScope' to ignore inactive records

---
+++ app/Models/Scopes/ExcludeInactiveScope.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class ExcludeInactiveScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $builder
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        return $builder->where('active', true);
+    }
+}
