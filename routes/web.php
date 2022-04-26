efea3794d09ec6744003eebdc76d36e97a709d53
Leonardo Yanes <leo95batista@gmail.com>
2022-04-26 08:15:30 -0400

    Register route 'logout'

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
+
+Route::middleware(config('fortify.middleware', ['web']))->group(function () {
+
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth:sanctum');
+
+});
